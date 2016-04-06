f0722a0963b79a58129d93c97a03e6f758a5acfd
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2016-04-06 13:32:30 -0400

    pass second test

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,5 @@
 Rails.application.routes.draw do
   root to: "todos#index"
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :todos, only: [:index, :new, :create]
 end
