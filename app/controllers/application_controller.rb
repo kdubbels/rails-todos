f4696e4a1306cbe3c4cc479fb30f01528ee7e4a9
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2016-06-12 22:51:38 -0400

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,12 @@ class ApplicationController < ActionController::Base
   def signed_in?
     session[:current_email].present?
   end
+
+  def current_email
+    session[:current_email]
+  end
+
+  def sign_in_as(email)
+    session[:current_email] = email
+  end
 end
