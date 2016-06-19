4b6d35568c9cc991b560742b09296684d42bc290
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2016-06-19 15:52:01 -0400

    puff puff pass

---
+++ spec/features/user_marks_todo_incomplete_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+feature "User marks todo incomplete" do
+  scenario "successfully" do
+  	sign_in
+
+    create_todo "Buy milk"
+
+    click_on "Mark complete"
+    click_on "Mark incomplete"
+
+    expect(page).not_to display_completed_todo "Buy milk"
+    expect(page).to display_todo "Buy milk"
+  end
+end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -24,3 +24,15 @@ describe Todo, "#complete!" do
     expect(todo).to be_completed
   end
 end
+
+describe Todo, "#mark_incomplete!" do
+  it "update completed_at to nil" do
+  	todo = Todo.create!(completed_at: Time.current)
+
+  	todo.mark_incomplete!
+  
+    todo.reload
+
+    expect(todo).not_to be_completed
+  end
+end
